fc5184809a81cbff4717613f6f52437f6775077c
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-18 22:27:29 +0530

    Coloring issues fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -3,48 +3,167 @@ import 'package:timezone/timezone.dart';
 import 'package:flutter/services.dart';
 import 'package:timezone/standalone.dart';
 
-
-void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primarySwatch: Colors.black,
-  ),
-  home : TimeZonesWorld()
-));
-
-class TimeZonesWorld extends StatefulWidget {
-  @override
-  _TimeZonesWorldState createState() => _TimeZonesWorldState();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var byteData =
+  await rootBundle.load('packages/timezone/data/$tzDataDefaultFilename');
+  initializeDatabase(byteData.buffer.asUint8List());
+  runApp(WorldTimeZone());
 }
 
-class _TimeZonesWorldState extends State<TimeZonesWorld> {
+class WorldTimeZone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        appBarTheme: AppBarTheme(color: Colors.black)
+      ),
+      home: HomePage(),
+    );
+  }
+}
 
+class HomePage extends StatefulWidget {
+  String location;
+  HomePage({this.location = 'Asia/Kolkata'});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+List allLocations = [];
+class _HomePageState extends State<HomePage> {
   @override
   void initState() {
-    // TODO: implement initState
+    initializeTimeZone();
+    timeZoneDatabase.locations.keys.forEach((x) => allLocations.add(x));
     super.initState();
   }
 
-  void setup() async {
-    LocationDatabase locationData = LocationDatabase();
-    var byteData = await rootBundle.load('packages/timezone/data/2019b.tzf');
-    initializeDatabase(byteData.buffer.asUint8List());
+  Future<void> initializeTimeZone() async {
+    await initializeTimeZone();
   }
-
-
-
+  String getTimeZone(String location) {
+    String sign =
+    TZDateTime.now(getLocation(location)).timeZoneOffset.isNegative
+        ? '-'
+        : '+';
+    int hour =
+    TZDateTime.now(getLocation(location)).timeZoneOffset.inHours.abs();
+    int minute = TZDateTime.now(getLocation(location)).timeZoneOffset.inMinutes;
+    return '$sign' + ' ' + '$hour' + ':' + '${minute % 60}';
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text("Select Time Zone"),
-          actions: <Widget>[
-            Icon(Icons.search,
-            color: Colors.white),
-          ],
+          title: Text('Select Time Zone'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(child: Text('Region', style: TextStyle(fontSize: 24, color: Colors.white70),)),
+                ),
+                subtitle:
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(child: Text('${widget.location}  ${getTimeZone(widget.location)}', style: TextStyle(fontSize: 24, color: Colors.white70),)),
+                ),
+                onTap: () async {
+                  dynamic newLocation = await showSearch(
+                    context: context,
+                    delegate: LocationsSearch(allLocations),
+                  );
+                  if (newLocation != null) {
+                    widget.location = newLocation;
+                    setState(() {});
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+  class LocationsSearch extends SearchDelegate<String> {
+  final List<dynamic> cities;
+
+  LocationsSearch(this.cities);
+
+  @override
+    ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return theme.copyWith(
+      primaryColor: theme.primaryColor,
+      primaryIconTheme: theme.primaryIconTheme,
+      primaryColorBrightness: theme.primaryColorBrightness,
+      primaryTextTheme: theme.primaryTextTheme,
+
+
+    );
+
+    }
+
+    @override
+    List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        onPressed: () {
+        query = '';
+        },
+        icon: Icon(Icons.close)),
+    ];
+  }
+
+    @override
+    Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+        },
+      );
+      }
+
+    @override
+    Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return Container();
+    }
+
+    @override
+    Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    List results = allLocations
+        .where((cityName) => cityName.toLowerCase().contains(query))
+        .toList();
+    return ListView.builder(
+     itemCount: results.length,
+     itemBuilder: (context, index) {
+     return ListTile(
+       onTap: () {
+          close(context, results[index]);
+          },
+      dense: true,
+      title: Text(results[index]),
+        );
+      },
+     );
+    }
+   }
+
+
+
 
--- lib/select_location_page.dart
+++ lib/select_location_page.dart
@@ -24,45 +24,42 @@ class _SelectLocationScreenState extends State<SelectLocationScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          backgroundColor: Colors.blue,
           title: TextField(
             controller: textController,
-            decoration: InputDecoration(
-              icon: Icon(
-                Icons.search,
-                color: Colors.blue,
-              ),
-              hintText: "'Search region (country/region)')",
-            ),
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.red),
+
           ),
         ),
-        body: ListView.builder(
-            itemCount: widget.allLocations.where((x) => x.contains(textController.text)).toList().length,
-            itemBuilder: (context, index){
-              return Container(
-                color: Colors.white,
-                child: ListTile(
-                  onTap: () {
-                    Navigator.push(
-                        (context),
-                        MaterialPageRoute(
-                            builder: (context) => HomePage(
-                              location: widget.allLocations
-                                  .where((x) => x.contains(textController.text))
-                                  .toList()[index],
-                            )
-                        )
-                    );
-                  },
-                  dense: true,
-                  title: Center(
-                    child: Text(
-                        widget.allLocations.where((x) => x.contains(textController.text)).toList()[index]),
+        body: Center(
+          child: ListView.builder(
+              itemCount: widget.allLocations.where((x) => x.contains(textController.text)).toList().length,
+              itemBuilder: (context, index){
+                return Container(
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          (context),
+                          MaterialPageRoute(
+                              builder: (context) => HomePage(
+                                location: widget.allLocations
+                                    .where((x) => x.contains(textController.text))
+                                    .toList()[index],
+                              )
+                          )
+                      );
+                    },
+                    dense: true,
+                    title: Center(
+                      child: Text(
+                          widget.allLocations.where((x) => x.contains(textController.text)).toList()[index]),
+                    ),
                   ),
-                ),
-              );
-            }
+                );
+              }
+          ),
         ),
       ),
     );
